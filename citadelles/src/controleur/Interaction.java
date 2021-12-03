3c2a50533feb33f53e57bfee6fbcbe6b59872eb1
karim216 <ahmedkarim.njiawouogbetnkom@esprit.tn>
2021-12-03 18:31:15 +0100

    commit test class interaction

---
+++ citadelles/src/controleur/Interaction.java
@@ -0,0 +1,88 @@
+package controleur;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Interaction {
+	private static Scanner sc = new Scanner(System.in);
+	private static Random rd = new Random();
+
+	public static int lireUnEntier() {
+		int i = 0;
+		boolean continu = true;
+		do {
+			try {
+				i = sc.nextInt();
+				continu = false;
+			} catch (InputMismatchException e) {
+				System.out.print("Veuillez rentrer un chiffre : ");
+				sc.next(); // passe l'entier pour éviter de boucler
+			}
+		} while(continu);
+		return i;
+	}
+
+	// renvoie un entier lu au clavier compris dans l'intervalle
+	//     [borneMin, borneMax[
+	public static int lireUnEntier(int borneMin, int borneMax) {
+		int i = 0;
+		// ...
+		boolean continu = true;
+		do {
+			i = lireUnEntier();
+			if(i>=0 && i<10) {
+				continu = false;
+			}
+			else {
+				//continu = true;
+				System.out.print("Veuillez rentrer un entier dans l'intevalle [0;10[: ");
+				//sc.next(); // passe l'entier pour éviter de boucler
+			}
+		}while(continu);
+		return i;
+	}
+
+	// lit les réponses "oui", "non", "o" ou "n" et renvoie un booléen
+	public static boolean lireOuiOuNon() {
+		boolean retour = true;
+		boolean continu = true;
+		// ...
+		do {
+			String reponse = lireUneChaine().toLowerCase();
+			//System.out.print(reponse);
+			if(reponse.equals("oui") == true || reponse.equals("o") == true) {
+				retour = true;
+				continu = false;
+			}
+			else if(reponse.equals("non") == true || reponse.equals("n") == true){
+				retour = false;
+				continu = false;
+			}
+			else {
+				System.out.print("Veuillez rentrer \"oui\", \"o\", \"non\" ou \"n\" : ");
+			}
+		}while(continu);
+		
+		return retour;
+	}
+
+	// renvoie une chaine de caractère lue au clavier:
+	public static String lireUneChaine() {
+		String retour = "";
+		boolean continu = true;
+		do {
+			try {
+				retour = sc.nextLine();
+				continu = false;
+			} catch (InputMismatchException e) {
+				System.out.print("Votre entrée doit être un caractère ou une chaine de caractère : ");
+				sc.next(); // passe l'entier pour éviter de boucler
+			}
+		} while(continu);
+		return retour;
+	}
+
+
+	
+}
