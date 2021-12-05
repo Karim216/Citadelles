d33c00fccf78f35af21dd54f3d8ef0914a7323f1
karim216 <ahmedkarim.njiawouogbetnkom@esprit.tn>
2021-12-05 01:24:01 +0100

    add class assassin

---
+++ citadelles/src/citadelles/modele/Assassin.java
@@ -0,0 +1,16 @@
+package citadelles.modele;
+
+public class Assassin extends Personnage{
+
+	public Assassin() {
+		super("Assassin", 1, Caracteristiques.ASSASSIN);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void utiliserPouvoir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
